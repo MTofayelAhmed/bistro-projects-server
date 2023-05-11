b9efaf7169527132b20dc2c7a97b160f8f11bf6f
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-12 02:51:54 +0600

    'update'

--- coffee-store-client/src/components/UpdateCoffee.jsx
+++ coffee-store-client/src/components/UpdateCoffee.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const UpdateCoffee = () => {
+  const coffee = useLoaderData()
   return (
     <div>
       <h2>this is update Coffee page</h2>
